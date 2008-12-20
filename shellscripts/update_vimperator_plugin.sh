d72351b1078d7fe99dd87d813d2b6722863f4d44
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2008-12-20 16:48:14 +0000

    Fix some problems.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@16 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- shellscripts/update_vimperator_plugin.sh
+++ shellscripts/update_vimperator_plugin.sh
@@ -3,7 +3,7 @@
 PLUGIN_PATH=$HOME/.vimperator/plugin
 PLUGIN_LIST=$HOME/.vimperator/GetLatest/GetLatestVimperatorPlugins.dat
 
-mkdir $PLUGIN_PATH\_tmp
+mkdir -p $PLUGIN_PATH\_tmp
 cd $PLUGIN_PATH\_tmp
 for i in `cat $PLUGIN_LIST`; do
   wget http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/`basename $i`
@@ -12,4 +12,3 @@ done
 rm -rf $PLUGIN_PATH\_old
 mv $PLUGIN_PATH $PLUGIN_PATH\_old
 mv $PLUGIN_PATH\_tmp $PLUGIN_PATH
-mv $PLUGIN_PATH\_old/.svn $PLUGIN_PATH
