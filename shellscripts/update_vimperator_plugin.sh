299db9187630dacad30bc1336c17092a5b86608a
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2008-12-20 16:35:44 +0000

    Remove vimperator plugins from repository.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@14 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- shellscripts/update_vimperator_plugin.sh
+++ shellscripts/update_vimperator_plugin.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 PLUGIN_PATH=$HOME/.vimperator/plugin
+PLUGIN_LIST=$HOME/.vimperator/GetLatest/GetLatestVimperatorPlugins.dat
 
 mkdir $PLUGIN_PATH\_tmp
 cd $PLUGIN_PATH\_tmp
-for i in `ls $PLUGIN_PATH`; do
+for i in `cat $PLUGIN_LIST`; do
   wget http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/`basename $i`
 done
 
 rm -rf $PLUGIN_PATH\_old
 mv $PLUGIN_PATH $PLUGIN_PATH\_old
 mv $PLUGIN_PATH\_tmp $PLUGIN_PATH
+mv $PLUGIN_PATH\_old/.svn $PLUGIN_PATH
