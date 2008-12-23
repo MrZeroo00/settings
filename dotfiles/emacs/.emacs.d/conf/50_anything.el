2c6aa5dcea757cc992982f596e256bfdd640fc06
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2008-12-23 19:14:40 +0000

    Update emacs setting.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@28 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

---
+++ dotfiles/emacs/.emacs.d/conf/50_anything.el
@@ -0,0 +1,13 @@
+(require 'anything-config)
+
+(setq anything-sources (list anything-c-source-buffers
+                             anything-c-source-bookmarks
+                             anything-c-source-man-pages
+                             anything-c-source-file-name-history
+                             anything-c-source-locate
+                             anything-c-source-complex-command-history))
+(anything-iswitchb-setup)
+
+(global-set-key "\C-xb" 'anything)
+(define-key anything-map (kbd "C-M-n") 'anything-next-source)
+(define-key anything-map (kbd "C-M-p") 'anything-previous-source)
