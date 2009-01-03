be3614efc4be92230cf2a0a0953ff14d1917a01c
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2009-01-03 11:46:48 +0000

    Add scheme-complete to Emacs.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@81 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/50_scheme.el
+++ dotfiles/emacs/.emacs.d/conf/50_scheme.el
@@ -4,3 +4,20 @@
 (setq scheme-program-name "gosh -i")
 (autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
 (autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
+
+
+;; scheme-complete
+(autoload 'scheme-smart-complete "scheme-complete" nil t)
+(eval-after-load 'scheme
+  '(progn
+     (define-key scheme-mode-map "\e\t" 'scheme-smart-complete)
+     (define-key scheme-mode-map "\t" 'scheme-complete-or-indent)))
+
+(autoload 'scheme-get-current-symbol-info "scheme-complete" nil t)
+(add-hook 'scheme-mode-hook
+          (lambda ()
+            (setq default-scheme-implementation 'gauche)
+            (setq *current-scheme-implementation* 'gauche)
+            (make-local-variable 'eldoc-documentation-function)
+            (setq eldoc-documentation-function 'scheme-get-current-symbol-info)
+            (eldoc-mode)))
