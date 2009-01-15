f405ee45a1c43999250d28370652678437f2a502
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2009-01-15 04:32:12 +0000

    Change default font size.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@165 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/00_local_linux.el
+++ dotfiles/emacs/.emacs.d/conf/00_local_linux.el
@@ -1,6 +1,6 @@
 (when window-system
   (progn
-    (set-default-font "Bitstream Vera Sans Mono")
+    (set-default-font "Bitstream Vera Sans Mono-10")
     (set-fontset-font (frame-parameter nil 'font)
                       'japanese-jisx0208
                       '("VL ゴシック" . "unicode-bmp"))))
