e722a497b09de187eb14b99b796929b81ec00bb2
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2009-01-19 15:43:30 +0000

    Trivial update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@185 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/40_coding.el
+++ dotfiles/emacs/.emacs.d/conf/40_coding.el
@@ -142,9 +142,11 @@
 
 (global-set-key "\C-cd" 'flymake-display-err-menu-for-current-line)
 
+; redefine to remove "check-syntax" target
 (defun flymake-get-make-cmdline (source base-dir)
   (list "make"
-        (list "-s" "-C"
+        (list "-s"
+              "-C"
               base-dir
               (concat "CHK_SOURCES=" source)
               "SYNTAX_CHECK_MODE=1")))
