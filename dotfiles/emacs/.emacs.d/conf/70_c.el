8d7b9246445f6c0d1c894d0af93b1cc4eef9e9fc
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2009-01-20 17:48:42 +0000

    Add flymake setting.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@197 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/70_c.el
+++ dotfiles/emacs/.emacs.d/conf/70_c.el
@@ -29,6 +29,16 @@
 
 
 ;; flymake
+(defun my-flymake-cc-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+         (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "gcc" (list "-Wall" "-Wextra" "-fsyntax-only" local-file))))
+(setcdr (assoc "\\.c\\'" flymake-allowed-file-name-masks)
+        '(my-flymake-cc-init))
+
 ;(add-hook 'c-mode-common-hook
 ;          '(lambda ()
 ;             (flymake-mode)))
