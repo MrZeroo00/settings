48adcff1f6cddbee1025c93a8d014981d930fae6
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2009-01-27 16:20:15 +0000

    Fix error.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@218 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/50_shell.el
+++ dotfiles/emacs/.emacs.d/conf/50_shell.el
@@ -49,13 +49,14 @@
 
 
 ;; eshell
+(autoload 'eshell "eshell" "Create an interactive Eshell buffer." t)
 (custom-set-variables
  '(eshell-ask-to-save-history (quote always))
  '(eshell-history-size 1000)
  '(eshell-ls-dired-initial-args (quote ("-h")))
  '(eshell-ls-exclude-regexp "~\\'")
  '(eshell-ls-initial-args "-h")
- '(eshell-ls-use-in-dired t nil (em-ls))
+ ;'(eshell-ls-use-in-dired t nil (em-ls))
  '(eshell-modules-list (quote (eshell-alias eshell-basic
                                             eshell-cmpl eshell-dirs eshell-glob
                                             eshell-hist eshell-ls eshell-pred
