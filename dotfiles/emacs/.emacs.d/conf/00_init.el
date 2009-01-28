f0e690d02640e4a65793d1ba39057263a295fcec
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2009-01-28 04:40:41 +0000

    Update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@224 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/00_init.el
+++ dotfiles/emacs/.emacs.d/conf/00_init.el
@@ -88,7 +88,7 @@
 ;; Etc
 (setq inhibit-startup-message t)
 (setq frame-title-format "%b")
-;(tool-bar-mode nil)
+(tool-bar-mode nil)
 ;(menu-bar-mode nil)
 (set-scroll-bar-mode 'right)
 (setq visible-bell t)
