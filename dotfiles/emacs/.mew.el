a4a4b660d781070d75f2f483eb2631634a671b43
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2009-01-31 15:06:22 +0000

    Add mew setting.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@231 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

---
+++ dotfiles/emacs/.mew.el
@@ -0,0 +1,17 @@
+(setq mew-mailbox-type 'mbox)
+(setq mew-mbox-command "incm")
+(setq mew-mbox-command-arg "-u -d ~/etc/mbox")
+
+
+(setq mew-proto "%")
+(setq mew-auto-get nil)
+(setq mew-decode-quoted t)
+(setq mew-use-fancy-thread t)
+(setq mew-thread-column 40)
+
+; Security
+(setq mew-use-cached-passwd t)
+(setq mew-use-master-passwd t)
+
+; POP
+(setq mew-pop-delete nil)
