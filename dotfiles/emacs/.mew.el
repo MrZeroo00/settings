9040f37d917d68a81c35d0176c34c6604b4af776
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2009-02-01 14:24:27 +0000

    Add gmail pop setting.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@237 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.mew.el
+++ dotfiles/emacs/.mew.el
@@ -7,25 +7,44 @@
 (setq mew-decode-quoted t)
 (setq mew-use-fancy-thread t)
 (setq mew-thread-column 40)
+(setq mew-prog-ssl (my-which "stunnel"))
+;(setq mew-ssl-verify-level 2)
+(setq mew-ssl-verify-level 0)
 
 ; Security
 (setq mew-use-cached-passwd t)
+(setq mew-passwd-timer-unit 60)
 (setq mew-use-master-passwd t)
 
 ; POP
+(setq mew-pop-size 0)
 (setq mew-pop-delete nil)
 
 ; Accounts
+(setq mew-name "Mitsuhiro Tanda")
 (setq mew-config-alist
       '(
         (gmail
          (mew-proto         "%")
+         (user              "mitsuhiro.tanda")
          (mail-domain       "gmail.com")
-         (imap-server       "imap.gmail.com")
-         (imap-ssl          t)
-         (imap-ssl-port     993)
-         (imap-user         "mitsuhiro.tanda@gmail.com")
-         (imap-auth         t)
-         (imap-delete       nil)
-         (imap-trash-folder "%trash"))
-        ))
+;         (mailbox-type      'mbox)
+;         (imap-server       "imap.gmail.com")
+;         (imap-ssl          t)
+;         (imap-ssl-port     993)
+;         (imap-user         "mitsuhiro.tanda@gmail.com")
+;         (imap-auth         t)
+;         (imap-delete       nil)
+;         (imap-trash-folder "%trash")
+         (mailbox-type      'pop)
+         (pop-server        "pop.gmail.com")
+         (pop-ssl           t)
+         (pop-ssl-port      995)
+         (pop-user          "mitsuhiro.tanda")
+         (pop-auth          pass)
+         (smtp-server       "smtp.gmail.com")
+         (smtp-ssl          t)
+         (smtp-ssl-port     465)
+         (smtp-user         "mitsuhiro.tanda")
+         )))
+(setq mew-case "gmail")
