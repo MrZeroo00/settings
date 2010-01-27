126479e442d417b6faacb70626877d585f120ef3
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2010-01-27 18:08:38 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@512 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.el
+++ dotfiles/emacs/.emacs.el
@@ -70,6 +70,23 @@
        (progn ,@body)
      (error (message "[eval-safe] %s" err))))
 
+;; define require and load
+(defmacro my-require-and-when (feature &rest body)
+  (declare (indent 1))
+  `(if (require ,feature nil t)
+       (progn
+         (message "Require success: %s" ,feature)
+         ,@body)
+     (message "Require error: %s" ,feature)))
+
+(defmacro my-load-and-when (name &rest body)
+  (declare (indent 1))
+  `(if (load ,name t)
+       (progn
+         (message "Load success: %s" ,name)
+         ,@body)
+     (message "Load error: %s" ,name)))
+
 
 (load "00_init")
 (load "01_util")
