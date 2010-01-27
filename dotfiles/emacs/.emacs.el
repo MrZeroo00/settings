6658341cf8f84e85af742e8c6e339683a3ef1f5c
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2010-01-27 18:06:00 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@511 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.el
+++ dotfiles/emacs/.emacs.el
@@ -62,30 +62,13 @@
 	(let ((default-directory dir))
 	  (load (expand-file-name "subdirs.el") t t t)))))
 
+
 ;; define eval-safe
 ;; http://www.sodan.org/~knagano/emacs/dotemacs.html#eval-safe
 (defmacro eval-safe (&rest body)
   `(condition-case err
        (progn ,@body)
      (error (message "[eval-safe] %s" err))))
-
-
-;; local settings
-(when run-linux
-  (progn
-    (if (file-exists-p "~/.emacs.d/conf/00_local_linux.el")
-	(load "00_local_linux"))))
-
-(when run-darwin
-  (progn
-    (add-to-list 'exec-path "/opt/local/bin")
-    (if (file-exists-p "~/.emacs.d/conf/00_local_darwin.el")
-	(load "00_local_darwin"))))
-
-(when (and run-w32 run-meadow)
-  (progn
-    (if (file-exists-p "~/.emacs.d/conf/00_local_meadow.el")
-	(load "00_local_meadow"))))
 
 
 (load "00_init")
@@ -148,11 +131,27 @@
 (load "99_anything")
 (load "99_hook")
 
+
+;; local settings
+(when run-linux
+  (progn
+    (if (file-exists-p "~/.emacs.d/conf/99_local_linux.el")
+	(load "99_local_linux"))))
+
+(when run-darwin
+  (progn
+    (add-to-list 'exec-path "/opt/local/bin")
+    (if (file-exists-p "~/.emacs.d/conf/99_local_darwin.el")
+	(load "99_local_darwin"))))
+
 (when (and run-w32 run-meadow)
-  (load "99_meadow"))
+  (progn
+    (if (file-exists-p "~/.emacs.d/conf/99_local_meadow.el")
+	(load "99_local_meadow"))))
 
 (if (file-exists-p "~/.emacs.d/conf/99_local.el")
     (load "99_local"))
+
 
 ;(if (y-or-n-p-with-timeout "Load timeout?" 5 nil)
 ;    (load "99_timeout"))
