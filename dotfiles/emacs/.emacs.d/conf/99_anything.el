24605ae98e4820497ce92e673d2ed319aeb4db4e
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2010-02-11 10:51:19 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@612 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/99_anything.el
+++ dotfiles/emacs/.emacs.d/conf/99_anything.el
@@ -200,10 +200,11 @@
 (setq anything-idle-delay 0.3)
 (setq anything-input-idle-delay 0)
 (setq anything-candidate-number-limit 100)
-;(setq anything-c-locate-db-file "~/home.locatedb")
-;(setq anything-c-locate-options `("locate" "-d" ,anything-c-locate-db-file "-i" "-r" "--"))
 (setq anything-candidate-separator
       "------------------------------------------------------------------------------------")
+(setq anything-enable-shortcuts 'alphabet)
+;(setq anything-c-locate-db-file "~/home.locatedb")
+;(setq anything-c-locate-options `("locate" "-d" ,anything-c-locate-db-file "-i" "-r" "--"))
 
 
 ; key setting
