051408a6a010f1383cae1060d1618d78c1255b11
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2010-02-13 16:44:38 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@642 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- shellscripts/emacs_update_elisp.sh
+++ shellscripts/emacs_update_elisp.sh
@@ -4,12 +4,12 @@ DIR='/tmp/update_elisp'
 mkdir ${DIR}
 cd ${DIR}
 
-for i in `grep -h "install-elisp-from-emacswiki" ${HOME}/.emacs.d/conf/* | perl -pe 's/.*"(.*)".*/\1/'`; do
+for i in `grep -h "install-elisp-from-emacswiki" ${HOME}/.emacs.d/conf/*.el | perl -pe 's/.*"(.*)".*/\1/'`; do
   wget -U Mozilla -t 1 "http://www.emacswiki.org/emacs/download/$i"
   sleep 1
 done
 
-for i in `grep -h "install-elisp" ${HOME}/.emacs.d/conf/* | grep -v "from-emacswiki" | perl -pe 's/.*"(.*)".*/\1/'`; do
+for i in `grep -h "install-elisp" ${HOME}/.emacs.d/conf/*.el | grep -v "from-emacswiki" | perl -pe 's/.*"(.*)".*/\1/'`; do
   wget -U Mozilla -t 1 "$i"
   sleep 1
 done
