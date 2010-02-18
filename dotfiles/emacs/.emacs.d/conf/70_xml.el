59af81faa60f0c862f7c398dff4036ff05c5fe1d
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2010-02-18 14:42:01 +0000

    remove tab related setting from each language setting.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@667 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/70_xml.el
+++ dotfiles/emacs/.emacs.d/conf/70_xml.el
@@ -4,9 +4,7 @@
             (lambda ()
               (setq nxml-slash-auto-complete-flag t)
               (setq nxml-child-indent 2)
-              (setq auto-fill-mode -1)
-              (setq indent-tabs-mode t)
-              (setq tab-width 2))))
+              (setq auto-fill-mode -1))))
 
 ;;;; association setting
 (add-to-list 'auto-mode-alist '("\\.\\(xml\\|xsl\\|rng\\|xhtml\\)\\'" . nxml-mode))
