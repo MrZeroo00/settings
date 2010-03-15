1b171723fb2df5170cfc4fc9601626e49deef9ec
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2010-03-15 14:20:36 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@792 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.el
+++ dotfiles/emacs/.emacs.el
@@ -1,6 +1,16 @@
 ;;;(setq debug-on-error t)
 
-(defun my-time-lag ()
+;;;; measure time for tuning
+(defconst my-time-zero (current-time))
+(defvar my-time-list nil)
+
+(defun my-time-lag-calc (lag label)
+  (if (assoc label my-time-list)
+      (setcdr (assoc label my-time-list)
+              (- lag (cdr (assoc label my-time-list))))
+    (setq my-time-list (cons (cons label lag) my-time-list))))
+
+(defun my-time-lag (label)
   (let* ((now (current-time))
          (min (- (car now) (car my-time-zero)))
          (sec (- (car (cdr now)) (car (cdr my-time-zero))))
@@ -8,8 +18,22 @@
                      (car (cdr (cdr my-time-zero))))
                   1000))
          (lag (+ (* 60000 min) (* 1000 sec) msec)))
-    (message "'.emacs.el' loading time: %d msec." lag)))
-(setq my-time-zero (current-time))
+    (my-time-lag-calc lag label)))
+
+(defun my-time-lag-print ()
+  (message (prin1-to-string
+            (sort my-time-list
+                  (lambda  (x y)  (> (cdr x) (cdr y)))))))
+(my-time-lag "total")
+(add-hook 'after-init-hook
+          (lambda () (my-time-lag "total")
+            (my-time-lag-print)
+            ;;(ad-disable-regexp 'require-time)
+            (switch-to-buffer
+             (get-buffer "*Messages*"))
+            ) t)
+(my-time-lag)
+
 
 ;;;; check Operating System
 (defvar run-unix
