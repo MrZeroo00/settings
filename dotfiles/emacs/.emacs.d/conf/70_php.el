287b401f4177b9b3029caebea8b250623c0e53ed
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-01-06 17:15:09 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@899 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/70_php.el
+++ dotfiles/emacs/.emacs.d/conf/70_php.el
@@ -11,3 +11,16 @@
                            (gtags-mode t)
                            ;;(gtags-make-complete-list)
                            ))
+
+
+;;;; php-completion
+;;;(install-elisp-from-emacswiki "php-completion.el")
+(add-hook 'php-mode-user-hook
+          (lambda ()
+            (my-require-and-when 'php-completion)
+            (php-completion-mode t)
+            (define-key php-mode-map (kbd "C-o") 'phpcmp-complete)
+            (when (require 'auto-complete nil t)
+              (make-variable-buffer-local 'ac-sources)
+              (add-to-list 'ac-sources 'ac-source-php-completion)
+              (auto-complete-mode t))))
