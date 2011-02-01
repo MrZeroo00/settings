9cd6a419f5ac0faf03a248ce28a5eaea4dee02ca
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-02-01 19:14:57 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@970 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/50_buffer.el
+++ dotfiles/emacs/.emacs.d/conf/50_buffer.el
@@ -1,5 +1,5 @@
 ;;;; uniquify (add directory name)
-(my-require-and-when 'uniquify
+'(my-require-and-when 'uniquify
   (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
   (setq uniquify-ignore-buffers-re "*[^*]+*"))
 
