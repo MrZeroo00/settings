7cc9f971095046fb125c2d365301b4f4b87e3d64
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-02-13 20:38:12 +0000

    cleanup.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1109 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/40_coding.el
+++ dotfiles/emacs/.emacs.d/conf/40_coding.el
@@ -278,23 +278,6 @@
   (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode))
 
 
-;;;; mode-info
-;;;; http://www.namazu.org/~tsuchiya/elisp/mode-info.html
-'(my-require-and-when 'mi-config
-;;;  (define-key global-map "\C-hf" 'mode-info-describe-function)
-;;;  (define-key global-map "\C-hv" 'mode-info-describe-variable)
-;;;  (define-key global-map "\M-." 'mode-info-find-tag)
-  (my-require-and-when 'mi-fontify)
-  (setq mode-info-index-directory "~/.emacs.d")
-  (setq mode-info-class-alist
-  '((elisp  emacs-lisp-mode lisp-interaction-mode)
-    (libc   c-mode c++-mode)
-    (make   makefile-mode)
-    (perl   perl-mode cperl-mode eperl-mode)
-    (ruby   ruby-mode)
-    (gauche scheme-mode scheme-interaction-mode inferior-scheme-mode))))
-
-
 ;;;; info-look
 (my-require-and-when 'info-look)
 
