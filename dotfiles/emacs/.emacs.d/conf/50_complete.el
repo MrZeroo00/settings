46634880e16adde987910b72207cc86e7fe2819a
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-02-13 12:37:23 +0000

    cleanup
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1081 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/50_complete.el
+++ dotfiles/emacs/.emacs.d/conf/50_complete.el
@@ -25,7 +25,7 @@
 (my-require-and-when 'auto-complete-config
 		     ;;(ac-config-default)
 		     (global-auto-complete-mode t)
-		     ;;(setq ac-auto-start 4)
+		     ;(setq ac-auto-start 4)
 		     (setq ac-auto-start nil)
 		     (setq ac-auto-show-menu 0.8)
 		     (setq ac-use-menu-map t)
@@ -82,61 +82,6 @@
 
 ;;;; macros
 ;;;(my-load-and-when "_maybe-capitalize-bos")
-
-
-;;;; obsolete
-'(progn
-;;;; dabbrev-highlight
-;;;(install-elisp "http://www.namazu.org/~tsuchiya/elisp/dabbrev-highlight.el")
-'(my-require-and-when 'dabbrev-highlight)
-
-
-;;;; dabbrev-ja
-;;;(install-elisp "http://namazu.org/%7Etsuchiya/elisp/dabbrev-ja.el")
-;;;(my-load-and-when "dabbrev-ja")
-
-
-;;;; abbrev-sort
-;;;(install-elisp "http://www.eskimo.com/~seldon/abbrev-sort.el")
-;;;(my-require-and-when 'abbrev-sort)
-
-
-;;;; ac-mode
-;;;(install-elisp "http://taiyaki.org/elisp/ac-mode/src/ac-mode.el")
-;;;(my-autoload-and-when 'ac-mode "ac-mode")
-
-
-;;;; mcomplete
-;;;(install-elisp "http://homepage1.nifty.com/bmonkey/emacs/elisp/mcomplete.el")
-;;;(my-require-and-when 'mcomplete)
-;;;(my-require-and-when 'cl)
-;;;(install-elisp "http://www.bookshelf.jp/elc/mcomplete-history.el")
-'(my-load-and-when "mcomplete-history"
-  (turn-on-mcomplete-mode))
-
-
-;;;; expand
-'(my-require-and-when 'expand
-  (expand-add-abbrevs c-mode-abbrev-table
-                      expand-c-sample-expand-list)
-  (expand-add-abbrevs lisp-interaction-mode-abbrev-table
-                      expand-sample-lisp-mode-expand-list)
-  (expand-add-abbrevs emacs-lisp-mode-abbrev-table
-                      expand-sample-lisp-mode-expand-list)
-  (expand-add-abbrevs lisp-mode-abbrev-table
-                      expand-sample-lisp-mode-expand-list)
-  (expand-add-abbrevs cperl-mode-abbrev-table
-                      expand-sample-perl-mode-expand-list))
-
-
-;;;; hippie-exp
-;;;(my-require-and-when 'hippie-exp)
-
-
-;;;; icicles
-;;;(install-elisp-from-emacswiki "icicles.el")
-;;;(my-require-and-when 'icicles)
-)
 
 
 ;; -*-no-byte-compile: t; -*-
---
+++ dotfiles/emacs/.emacs.d/conf/_obsolete.el
@@ -0,0 +1,56 @@
+;;;; 50_complete
+'(progn
+;;;; dabbrev-highlight
+;;;(install-elisp "http://www.namazu.org/~tsuchiya/elisp/dabbrev-highlight.el")
+'(my-require-and-when 'dabbrev-highlight)
+
+
+;;;; dabbrev-ja
+;;;(install-elisp "http://namazu.org/%7Etsuchiya/elisp/dabbrev-ja.el")
+;;;(my-load-and-when "dabbrev-ja")
+
+
+;;;; abbrev-sort
+;;;(install-elisp "http://www.eskimo.com/~seldon/abbrev-sort.el")
+;;;(my-require-and-when 'abbrev-sort)
+
+
+;;;; ac-mode
+;;;(install-elisp "http://taiyaki.org/elisp/ac-mode/src/ac-mode.el")
+;;;(my-autoload-and-when 'ac-mode "ac-mode")
+
+
+;;;; mcomplete
+;;;(install-elisp "http://homepage1.nifty.com/bmonkey/emacs/elisp/mcomplete.el")
+;;;(my-require-and-when 'mcomplete)
+;;;(my-require-and-when 'cl)
+;;;(install-elisp "http://www.bookshelf.jp/elc/mcomplete-history.el")
+'(my-load-and-when "mcomplete-history"
+  (turn-on-mcomplete-mode))
+
+
+;;;; expand
+'(my-require-and-when 'expand
+  (expand-add-abbrevs c-mode-abbrev-table
+                      expand-c-sample-expand-list)
+  (expand-add-abbrevs lisp-interaction-mode-abbrev-table
+                      expand-sample-lisp-mode-expand-list)
+  (expand-add-abbrevs emacs-lisp-mode-abbrev-table
+                      expand-sample-lisp-mode-expand-list)
+  (expand-add-abbrevs lisp-mode-abbrev-table
+                      expand-sample-lisp-mode-expand-list)
+  (expand-add-abbrevs cperl-mode-abbrev-table
+                      expand-sample-perl-mode-expand-list))
+
+
+;;;; hippie-exp
+;;;(my-require-and-when 'hippie-exp)
+
+
+;;;; icicles
+;;;(install-elisp-from-emacswiki "icicles.el")
+;;;(my-require-and-when 'icicles)
+)
+
+
+;; -*-no-byte-compile: t; -*-
