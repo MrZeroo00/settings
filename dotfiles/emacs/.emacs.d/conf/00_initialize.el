676fd02506d46dfcf6fc0932bdc70a37af0fe9fb
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-02-13 21:54:48 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1120 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/00_initialize.el
+++ dotfiles/emacs/.emacs.d/conf/00_initialize.el
@@ -26,9 +26,29 @@
 ;;;(setq default-buffer-file-coding-system 'utf-8)
 ;;;(set-file-name-coding-system 'utf-8)
 '(add-hook 'find-file-hooks
-		  (lambda ()
-			(setq coding-system-for-read 'utf-8)
-			(setq coding-system-for-write 'utf-8)))
+           (lambda ()
+             (setq coding-system-for-read 'utf-8)
+             (setq coding-system-for-write 'utf-8)))
+(cond
+ (run-darwin
+  (my-require-and-when 'ucs-normalize
+    (setq file-name-coding-system 'utf-8-hfs)
+    (setq locale-coding-system 'utf-8-hfs))
+  )
+ ((or run-w32 cygwin)
+  (setq file-name-coding-system 'utf-8)
+  (setq locale-coding-system 'utf-8)
+  ;;if use command prompt
+  ;;(setq file-name-coding-system 'sjis)
+  ;;(setq locale-coding-system 'sjis)
+  ;;if use old Cygwin
+  ;;(setq file-name-coding-system 'euc-jp)
+  ;;(setq locale-coding-system 'euc-jp)
+  )
+ (t
+  (setq file-name-coding-system 'utf-8)
+  (setq locale-coding-system 'utf-8)
+  ))
 
 
 ;;;; locale setting
--- dotfiles/emacs/.emacs.d/conf/50_shell.el
+++ dotfiles/emacs/.emacs.d/conf/50_shell.el
@@ -1,8 +1,21 @@
+(defun skt:shell ()
+  (or (executable-find "zsh")
+      (executable-find "bash")
+      ;;(executable-find "f_zsh") ;; Emacs + Cygwin を利用する人は Zsh の代りにこれにしてください
+      ;;(executable-find "f_bash") ;; Emacs + Cygwin を利用する人は Bash の代りにこれにしてください
+      (executable-find "cmdproxy")
+      (error "can't find 'shell' command in PATH!!")))
+(setq shell-file-name (skt:shell))
+(setenv "SHELL" shell-file-name)
+(setq explicit-shell-file-name shell-file-name)
+'(global-set-key (kbd "C-c t") '(lambda ()
+                                 (interactive)
+                                 (term shell-file-name)))
+
+(setq system-uses-terminfo nil)
 (my-autoload-and-when 'ansi-color-for-comint-mode-on "ansi-color")
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-
-(add-hook 'comint-output-filter-functions
-          'comint-watch-for-password-prompt)
+(add-hook 'comint-output-filter-functions 'comint-watch-for-password-prompt)
 
 (setq comint-scroll-show-maximum-output t)
 ;;;(setq explicit-shell-file-name "bash")
