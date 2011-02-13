fdbcbf19659616b7d222ba30343363704cd513e7
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-02-13 22:03:52 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1121 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/50_shell.el
+++ dotfiles/emacs/.emacs.d/conf/50_shell.el
@@ -9,8 +9,8 @@
 (setenv "SHELL" shell-file-name)
 (setq explicit-shell-file-name shell-file-name)
 '(global-set-key (kbd "C-c t") '(lambda ()
-                                 (interactive)
-                                 (term shell-file-name)))
+                                  (interactive)
+                                  (term shell-file-name)))
 
 (setq system-uses-terminfo nil)
 (my-autoload-and-when 'ansi-color-for-comint-mode-on "ansi-color")
@@ -26,18 +26,43 @@
 
 ;;;; anything
 '(add-hook 'shell-mode-hook
-          (lambda ()
-            (make-variable-buffer-local 'anything-sources)
-            (add-to-list 'anything-sources 'anything-c-source-complete-shell-history)))
+           (lambda ()
+             (make-variable-buffer-local 'anything-sources)
+             (add-to-list 'anything-sources 'anything-c-source-complete-shell-history)))
+
+
+;;;; multi-term
+;;;(install-elisp-from-emacswiki "multi-term.el")
+'(my-require-and-when 'multi-term
+  (setq multi-term-program shell-file-name)
+  (add-to-list 'term-unbind-key-list '"M-x")
+  (global-set-key (kbd "C-c t") '(lambda ()
+                                   (interactive)
+                                   (multi-term)))
+  '(global-set-key (kbd "C-c t") '(lambda ()
+                                   (interactive)
+                                   (if (get-buffer "*terminal<1>*")
+                                       (switch-to-buffer "*terminal<1>*")
+                                     (multi-term))))
+  (global-set-key (kbd "C-c n") 'multi-term-next)
+  (global-set-key (kbd "C-c p") 'multi-term-prev)
+  )
+'(add-hook 'term-mode-hook
+          '(lambda ()
+             (define-key term-raw-map (kbd "C-h") 'term-send-backspace)
+             (define-key term-raw-map (kbd "C-y") 'term-paste)
+             ))
 
 
 ;;;; shell-pop
 ;;;(install-elisp-from-emacswiki "shell-pop.el")
 '(my-require-and-when 'shell-pop
-  (shell-pop-set-internal-mode "ansi-term")
-  (shell-pop-set-internal-mode-shell "/bin/zsh")
-  (shell-pop-set-window-height 60) ; the number for the percentage of the selected window.
-  )
+   (add-to-list 'shell-pop-internal-mode-list '("multi-term" "*terminal<1>*" '(lambda () (multi-term))))
+   (shell-pop-set-internal-mode "multi-term")
+   '(shell-pop-set-internal-mode "ansi-term")
+   (shell-pop-set-internal-mode-shell shell-file-name)
+   (shell-pop-set-window-height 60) ; the number for the percentage of the selected window.
+   )
 ;;;(global-set-key [f8] 'shell-pop)
 
 
@@ -48,14 +73,14 @@
 
 ;;;; shell-command
 ;;;(install-elisp "http://namazu.org/~tsuchiya/elisp/shell-command.el")
-'(my-require-and-when 'shell-command ; replaced by anything
-  (shell-command-completion-mode))
+'(my-require-and-when 'shell-command    ; replaced by anything
+   (shell-command-completion-mode))
 
 
 ;;;; background
 ;;;(install-elisp "http://www.jgk.org/src/background.el")
 '(my-autoload-and-when 'background "background"
-                      (global-set-key "\M-!" 'background))
+   (global-set-key "\M-!" 'background))
 
 
 ;;;; pcomplete
@@ -77,7 +102,7 @@
 (add-hook 'shell-mode-hook
           (lambda ()
             (setq outline-regexp "[^ ]*[>%#]")
-;;;            (outline-minor-mode t)
+            '(outline-minor-mode t)
             (turn-on-orgstruct)))
 
 
