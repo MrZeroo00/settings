e7021ccab7fc408cfe068b0b0839908a30db5188
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-02-13 16:58:38 +0000

    cleanup.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1095 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/70_ruby.el
+++ dotfiles/emacs/.emacs.d/conf/70_ruby.el
@@ -1,83 +1,89 @@
-(autoload 'ruby-mode "ruby-mode" "Mode for editing ruby source files" t)
-(autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process")
-(autoload 'inf-ruby-keys "inf-ruby" "Set local key defs for inf-ruby in ruby-mode")
+(my-autoload-and-when 'ruby-mode "ruby-mode"
+  ;; flymake
+  (when (featurep 'flymake)
+    (defun flymake-ruby-init ()
+      (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                           'flymake-create-temp-inplace))
+             (local-file  (file-relative-name
+                           temp-file
+                           (file-name-directory buffer-file-name))))
+        (list "ruby" (list "-c" local-file))))
+    (add-to-list 'flymake-allowed-file-name-masks
+                 '(".+\\.rb$" flymake-ruby-init))
+
+    )
+
+  ;; align
+  '(progn
+     (add-to-list 'align-rules-list
+                  '(ruby-comma-delimiter
+                    (regexp . ",\\(\\s-*\\)[^# \t\n]")
+                    (repeat . t)
+                    (modes  . '(ruby-mode))))
+     (add-to-list 'align-rules-list
+                  '(ruby-hash-literal
+                    (regexp . "\\(\\s-*\\)=>\\s-*[^# \t\n]")
+                    (repeat . t)
+                    (modes  . '(ruby-mode))))
+     (add-to-list 'align-rules-list
+                  '(ruby-assignment-literal
+                    (regexp . "\\(\\s-*\\)=\\s-*[^# \t\n]")
+                    (repeat . t)
+                    (modes  . '(ruby-mode))))
+     (add-to-list 'align-rules-list     ;TODO add to rcodetools.el
+                  '(ruby-xmpfilter-mark
+                    (regexp . "\\(\\s-*\\)# => [^#\t\n]")
+                    (repeat . nil)
+                    (modes  . '(ruby-mode))))
+     )
+
+  ;; macros
+  (my-load-and-when "_ruby-insert-magic-comment-if-needed")
+  )
+(my-autoload-and-when 'run-ruby "inf-ruby"
+  )
+(my-autoload-and-when 'inf-ruby-keys "inf-ruby"
+  )
+
 
 ;;;; association setting
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
 
-(setq ruby-deep-indent-paren-style nil)
 
+;;;; mode hook
+(defun my-ruby-mode-hook ()
+  ;; common setting
+  (setq ruby-deep-indent-paren-style nil)
 
-;;;; auto-complete
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (make-local-variable 'ac-ignores)
-            (add-to-list 'ac-ignores "end")))
+  ;; auto-complete
+  (when (featurep 'auto-complete)
+    (make-local-variable 'ac-ignores)
+    (add-to-list 'ac-ignores "end")
+    )
 
+  ;; flymake
+  (when (featurep 'flymake)
+    (inf-ruby-keys)
+    (flymake-mode t)
+    )
 
-;;;; flymake
-(defun flymake-ruby-init ()
-  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-   (local-file  (file-relative-name
-                       temp-file
-                       (file-name-directory buffer-file-name))))
-    (list "ruby" (list "-c" local-file))))
-(add-to-list 'flymake-allowed-file-name-masks
-             '(".+\\.rb$" flymake-ruby-init))
+  ;; autotest
+  ;;(install-elisp-from-emacswiki "autotest.el")
+  (my-require-and-when 'autotest)
 
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (inf-ruby-keys)
-            (flymake-mode t)))
+  ;; refe
+  ;;(install-elisp "http://ns103.net/~arai/ruby/refe.el")
+  '(my-require-and-when 'refe)
+  ;; http://d.hatena.ne.jp/rubikitch/20071228/rubyrefm
+  ;;(install-elisp "http://www.rubyist.net/~rubikitch/archive/refe2.e")
+  (my-load-and-when "_refe2")
 
-
-;;;; refe
-;;;(install-elisp "http://ns103.net/~arai/ruby/refe.el")
-;;;(my-require-and-when 'refe)
-;;;; http://d.hatena.ne.jp/rubikitch/20071228/rubyrefm
-;;;(install-elisp "http://www.rubyist.net/~rubikitch/archive/refe2.e")
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (my-load-and-when "_refe2")))
-
-
-;;;; autotest
-;;;(install-elisp-from-emacswiki "autotest.el")
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (my-require-and-when 'autotest)))
-
-
-;;;; align
-;;;(add-to-list 'align-rules-list
-;;;             '(ruby-comma-delimiter
-;;;               (regexp . ",\\(\\s-*\\)[^# \t\n]")
-;;;               (repeat . t)
-;;;               (modes  . '(ruby-mode))))
-;;;(add-to-list 'align-rules-list
-;;;             '(ruby-hash-literal
-;;;               (regexp . "\\(\\s-*\\)=>\\s-*[^# \t\n]")
-;;;               (repeat . t)
-;;;               (modes  . '(ruby-mode))))
-;;;(add-to-list 'align-rules-list
-;;;             '(ruby-assignment-literal
-;;;               (regexp . "\\(\\s-*\\)=\\s-*[^# \t\n]")
-;;;               (repeat . t)
-;;;               (modes  . '(ruby-mode))))
-;;;(add-to-list 'align-rules-list          ;TODO add to rcodetools.el
-;;;             '(ruby-xmpfilter-mark
-;;;               (regexp . "\\(\\s-*\\)# => [^#\t\n]")
-;;;               (repeat . nil)
-;;;               (modes  . '(ruby-mode))))
-
-
-;;;; macros
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (my-load-and-when "_ruby-insert-magic-comment-if-needed")
-            (add-hook 'before-save-hook 'ruby-insert-magic-comment-if-needed)))
+  ;; macros
+  (when (fboundp 'ruby-insert-magic-comment-if-needed)
+    (add-hook 'before-save-hook 'ruby-insert-magic-comment-if-needed)
+    )
+  )
 
 
 ;; -*-no-byte-compile: t; -*-
--- dotfiles/emacs/.emacs.d/conf/70_scheme.el
+++ dotfiles/emacs/.emacs.d/conf/70_scheme.el
@@ -1,29 +1,35 @@
 (my-autoload-and-when 'scheme-mode "cmuscheme")
 (my-autoload-and-when 'run-scheme "cmuscheme")
 
-;;;; mode hook
-(my-eval-after-load "cmuscheme"
-  (add-hook 'scheme-mode-hook
-            (lambda ()
-              (setq default-scheme-implementation 'gauche)
-              (setq *current-scheme-implementation* 'gauche)
-              (make-local-variable 'eldoc-documentation-function)
-              (setq eldoc-documentation-function 'scheme-get-current-symbol-info)
-              (eldoc-mode)))
-
-  (setq process-coding-system-alist
-        (cons '("gosh" utf-8 . utf-8) process-coding-system-alist))
-
-  (setq scheme-program-name "gosh -i"))
-
 
 ;;;; scheme-complete
 (my-autoload-and-when 'scheme-smart-complete "scheme-complete")
 (my-autoload-and-when 'scheme-get-current-symbol-info "scheme-complete")
-(add-hook 'scheme-mode-hook
-          (lambda ()
-            (define-key scheme-mode-map "\e\t" 'scheme-smart-complete)
-            (define-key scheme-mode-map "\t" 'scheme-complete-or-indent)))
+
+
+;;;; mode hook
+(defun my-scheme-mode-hook ()
+  ;; common setting
+  (when (featurep 'cmuscheme)
+    (setq default-scheme-implementation 'gauche)
+    (setq *current-scheme-implementation* 'gauche)
+    (make-local-variable 'eldoc-documentation-function)
+    (setq eldoc-documentation-function 'scheme-get-current-symbol-info)
+    (eldoc-mode)
+
+    (setq process-coding-system-alist
+          (cons '("gosh" utf-8 . utf-8) process-coding-system-alist))
+
+    (setq scheme-program-name "gosh -i")
+    )
+
+  ;; scheme-complete
+  (when (featurep 'scheme-complete)
+    (define-key scheme-mode-map "\e\t" 'scheme-smart-complete)
+    (define-key scheme-mode-map "\t" 'scheme-complete-or-indent)
+    )
+  )
+(add-hook 'scheme-mode-hook 'my-scheme-mode-hook)
 
 
 ;; -*-no-byte-compile: t; -*-
