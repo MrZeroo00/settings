881cc7d706d2002ab0328325f9b8bacb54159f75
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-02-13 12:41:23 +0000

    cleanup
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1082 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/40_etc.el
+++ dotfiles/emacs/.emacs.d/conf/40_etc.el
@@ -1,26 +1,3 @@
-;;;; Outputz
-;;;(install-elisp "http://svn.coderepos.org/share/lang/elisp/outputz/outputz.el")
-'(my-require-and-when 'outputz
-  (setq outputz-key "Your Private Key") ;; 復活の呪文
-  (setq outputz-uri "http://example.com/%s") ;; 適当なURL。%sにmajor-modeの名前が入るので、major-modeごとのURLで投稿できます。
-  (global-outputz-mode t)
-
-  (defun outputz-buffers ()
-    (dolist (buf (buffer-list))
-      (with-current-buffer buf
-        (outputz))))
-
-  (run-with-idle-timer 3 t 'outputz-buffers)
-  (remove-hook 'after-save-hook 'outputz))
-
-
-;;;; typing-outputz
-;;;; http://github.com/hayamiz/typing-outputz/tree/master
-'(my-require-and-when 'typing-outputz
-  (global-typing-outputz-mode t)
-  (setq typing-outputz-key "bWAROb-quUbf"))
-
-
 ;;;; pdicv-mode
 ;;;; http://pdicviewer.naochan.com/el/
 ;;;(my-require-and-when 'pdicv-search)
