375a58a44de2d9214bdbab9f5604448047f48ffa
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-02-13 16:57:22 +0000

    cleanup.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1094 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/70_text.el
+++ dotfiles/emacs/.emacs.d/conf/70_text.el
@@ -1,52 +1,18 @@
-;;;; mode hook
-(add-hook 'text-mode-hook
-          (lambda ()
-            (progn
-;;;              (ruler-mode)
-              (flyspell-mode)
-;;;              (refill-mode t)
-              )))
-
-;;;; common setting
-(setq paragraph-start '"^\\([ 　・○<\t\n\f]\\|(?[0-9a-zA-Z]+)\\)")
-
-
 ;;;; imenu
-(add-hook 'text-mode
-          (lambda ()
-            (progn
-              (setq imenu-create-index-function
-                  (lambda ()
-                    (let (index)
-                      (goto-char (point-min))
-                      (while (re-search-forward "^\*\s*\\(.+\\)" (point-max) t)
-                        (push (cons (match-string 1) (match-beginning 1)) index))
-    (nreverse index)))))))
+'(setq imenu-create-index-function
+      (lambda ()
+        (let (index)
+          (goto-char (point-min))
+          (while (re-search-forward "^\*\s*\\(.+\\)" (point-max) t)
+            (push (cons (match-string 1) (match-beginning 1)) index))
+          (nreverse index))))
 
 
 ;;;; word-count
 ;;;(install-elisp "http://taiyaki.org/elisp/word-count/src/word-count.el")
-(my-autoload-and-when 'word-count-mode "word-count"
-;;;                      (global-set-key "\M-+" 'word-count-mode)
-                      )
-
-
-;;;; text-adjust
-;;;(install-elisp "http://taiyaki.org/elisp/text-adjust/src/text-adjust.el")
-(add-hook 'text-mode-hook
-	  (lambda ()
-	    (my-require-and-when 'text-adjust
-				 (setq adaptive-fill-regexp "[ \t]*")
-				 (setq adaptive-fill-mode t)
-				 (setq text-adjust-touten-from nil)
-				 (setq text-adjust-kuten-from nil))))
-
-;;;(defun text-adjust-space-before-save-if-needed ()
-;;;  (when (memq major-mode
-;;;              '(org-mode text-mode mew-draft-mode))
-;;;    (text-adjust-space-buffer)))
-;;;(defalias 'spacer 'text-adjust-space-buffer)
-;;;(add-hook 'before-save-hook 'text-adjust-space-before-save-if-needed)
+'(my-autoload-and-when 'word-count-mode "word-count"
+  (global-set-key "\M-+" 'word-count-mode)
+  )
 
 
 ;;;; manued
@@ -54,10 +20,32 @@
 ;;;(my-autoload-and-when 'manued-minor-mode "manued")
 
 
-;;;; macros
-;;;(add-hook 'text-mode-hook
-;;;          (lambda ()
-;;;            (my-load-and-when "text-adjust-space-before-save-if-needed")))
+;;;; mode hook
+(defun my-text-mode-hook ()
+  ;; common setting
+  '(ruler-mode)
+  (flyspell-mode)
+  '(refill-mode t)
+  '(setq paragraph-start '"^\\([ 　・○<\t\n\f]\\|(?[0-9a-zA-Z]+)\\)")
+
+  ;; text-adjust
+  ;;(install-elisp "http://taiyaki.org/elisp/text-adjust/src/text-adjust.el")
+  '(my-require-and-when 'text-adjust
+     (setq adaptive-fill-regexp "[ \t]*")
+     (setq adaptive-fill-mode t)
+     (setq text-adjust-touten-from nil)
+     (setq text-adjust-kuten-from nil))
+  '(defun text-adjust-space-before-save-if-needed ()
+     (when (memq major-mode
+                 '(org-mode text-mode mew-draft-mode))
+       (text-adjust-space-buffer)))
+  '(defalias 'spacer 'text-adjust-space-buffer)
+  '(add-hook 'before-save-hook 'text-adjust-space-before-save-if-needed)
+
+  ;; macros
+  '(my-load-and-when "text-adjust-space-before-save-if-needed")
+  )
+(add-hook 'text-mode-hook 'my-text-mode-hook)
 
 
 ;; -*-no-byte-compile: t; -*-
--- dotfiles/emacs/.emacs.d/conf/70_yaml.el
+++ dotfiles/emacs/.emacs.d/conf/70_yaml.el
@@ -1,14 +1,17 @@
 ;;;; http://yaml-mode.clouder.jp/
-(my-autoload-and-when 'yaml-mode "yaml-mode")
+(my-autoload-and-when 'yaml-mode "yaml-mode"
+  )
+
 
 ;;;; association setting
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
+
 ;;;; mode hook
-(my-eval-after-load "yaml-mode"
-  (add-hook 'yaml-mode-hook
-            (lambda ()
-              (define-key yaml-mode-map "\C-m" 'newline-and-indent))))
+(defun my-yaml-mode-hook ()
+  (define-key yaml-mode-map "\C-m" 'newline-and-indent)
+  )
+(add-hook 'yaml-mode-hook 'my-yaml-mode-hook)
 
 
 ;; -*-no-byte-compile: t; -*-
