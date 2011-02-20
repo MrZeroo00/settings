79361af50ebce119ae308ef09098722b77a03323
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-02-20 15:51:41 +0000

    cleanup
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1213 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/50_dired.el
+++ dotfiles/emacs/.emacs.d/conf/50_dired.el
@@ -1,6 +1,6 @@
 (setq dired-recursive-copies 'always)
 (setq dired-recursive-deletes 'always)
-;;;(setq dired-listing-switches "-AFl")
+'(setq dired-listing-switches "-AFl")
 (setq dired-listing-switches "-Alph")
 (setq find-ls-option '("-exec ls -AFGl {} \\;" . "-AFGl"))
 (setq grep-find-command "find . -type f -print0 | xargs -0 -e grep -ns ")
@@ -30,72 +30,72 @@
 (my-require-and-when 'dired-x)
 
 '(setq dired-guess-shell-alist-user
-      '(("\\.tar\\.gz\\'" "tar ztvf")
-        ("\\.taz\\'" "tar ztvf")
-        ("\\.tar\\.bz2\\'" "tar Itvf")
-        ("\\.zip\\'" "unzip -l")
-        ("\\.\\(g\\|\\) z\\'" "zcat")
-        ("\\.\\(jpg\\|JPG\\|gif\\|GIF\\)\\'"
-         (if (eq system-type 'windows-nt)
-             "fiber" "xv"))
-        ("\\.ps\\'"
-         (if (eq system-type 'windows-nt)
-             "fiber" "ghostview"))
-        ))
+       '(("\\.tar\\.gz\\'" "tar ztvf")
+         ("\\.taz\\'" "tar ztvf")
+         ("\\.tar\\.bz2\\'" "tar Itvf")
+         ("\\.zip\\'" "unzip -l")
+         ("\\.\\(g\\|\\) z\\'" "zcat")
+         ("\\.\\(jpg\\|JPG\\|gif\\|GIF\\)\\'"
+          (if (eq system-type 'windows-nt)
+              "fiber" "xv"))
+         ("\\.ps\\'"
+          (if (eq system-type 'windows-nt)
+              "fiber" "ghostview"))
+         ))
 
 
 ;;;; my-dired-mode
 ;;;(install-elisp "http://www.bookshelf.jp/elc/my-dired-mode.el")
-;;;(my-load-and-when "my-dired-mode")
+'(my-load-and-when "my-dired-mode")
 
 
 ;;;; wdired (rename file name from dired buffer)
 ;;;(install-elisp-from-emacswiki "wdired.el")
 (my-require-and-when 'wdired
-;;;  (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
+  '(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
   )
 
 
 ;;;; bf-mode (show file content)
 ;;;(install-elisp "http://www.bookshelf.jp/elc/bf-mode.el")
 '(my-require-and-when 'bf-mode
-  (setq bf-mode-browsing-size 10)
-;;;  (setq bf-mode-except-ext '("\\.exe$" "\\.com$"))
-;;;  (setq bf-mode-force-browse-exts
-  ;;      (append '("\\.texi$" "\\.el$")
-  ;;              bf-mode-force-browse-exts))
-  (setq bf-mode-html-with-w3m t)
-  (setq bf-mode-archive-list-verbose t)
-  (setq bf-mode-directory-list-verbose t))
+   (setq bf-mode-browsing-size 10)
+   (setq bf-mode-except-ext '("\\.exe$" "\\.com$"))
+   (setq bf-mode-force-browse-exts
+         (append '("\\.texi$" "\\.el$")
+                 bf-mode-force-browse-exts))
+   (setq bf-mode-html-with-w3m t)
+   (setq bf-mode-archive-list-verbose t)
+   (setq bf-mode-directory-list-verbose t))
 
 
 ;;;; http://www.bookshelf.jp/cgi-bin/goto.cgi?file=meadow&node=dired%20single
-;;;(defvar my-dired-before-buffer nil)
-;;;(defadvice dired-advertised-find-file
-;;;  (before kill-dired-buffer activate)
-;;;  (setq my-dired-before-buffer (current-buffer)))
-;;;
-;;;(defadvice dired-advertised-find-file
-;;;  (after kill-dired-buffer-after activate)
-;;;  (if (eq major-mode 'dired-mode)
-;;;      (kill-buffer my-dired-before-buffer)))
-;;;
-;;;(defadvice dired-up-directory
-;;;  (before kill-up-dired-buffer activate)
-;;;  (setq my-dired-before-buffer (current-buffer)))
-;;;
-;;;(defadvice dired-up-directory
-;;;  (after kill-up-dired-buffer-after activate)
-;;;  (if (eq major-mode 'dired-mode)
-;;;      (kill-buffer my-dired-before-buffer)))
+'(defvar my-dired-before-buffer nil)
+'(defadvice dired-advertised-find-file
+   (before kill-dired-buffer activate)
+   (setq my-dired-before-buffer (current-buffer)))
+
+'(defadvice dired-advertised-find-file
+   (after kill-dired-buffer-after activate)
+   (if (eq major-mode 'dired-mode)
+       (kill-buffer my-dired-before-buffer)))
+
+'(defadvice dired-up-directory
+   (before kill-up-dired-buffer activate)
+   (setq my-dired-before-buffer (current-buffer)))
+
+'(defadvice dired-up-directory
+   (after kill-up-dired-buffer-after activate)
+   (if (eq major-mode 'dired-mode)
+       (kill-buffer my-dired-before-buffer)))
 
 
 ;;;; http://d.hatena.ne.jp/higepon/20061230/1167447340
-;;;(defadvice dired-sort-other
-;;;  (around dired-sort-other-h activate)
-;;;  (ad-set-arg 0 (concat (ad-get-arg 0) "h"))
-;;;  ad-do-it
-;;;  (setq dired-actual-switches (dired-replace-in-string "h" "" dired-actual-switches)))
+'(defadvice dired-sort-other
+   (around dired-sort-other-h activate)
+   (ad-set-arg 0 (concat (ad-get-arg 0) "h"))
+   ad-do-it
+   (setq dired-actual-switches (dired-replace-in-string "h" "" dired-actual-switches)))
 
 
 ;;;; macros
@@ -103,22 +103,19 @@
   (define-key dired-mode-map " " 'dired-toggle-mark))
 '(my-load-and-when "_dired-convert-coding-system")
 '(my-load-and-when "_dired-ps-print-files")
-;;;(my-load-and-when "_ls-lisp-handle-switches")
-;;;(my-load-and-when "_dired-face-file-edited-today")
+'(my-load-and-when "_ls-lisp-handle-switches")
+'(my-load-and-when "_dired-face-file-edited-today")
 (when (and run-w32 run-meadow)
   ;; w32-symlinks
-;;;     (install-elisp "http://centaur.maths.qmw.ac.uk/Emacs/files/w32-symlinks.el")
+  ;;(install-elisp "http://centaur.maths.qmw.ac.uk/Emacs/files/w32-symlinks.el")
   (my-require-and-when 'w32-symlinks)
   (my-load-and-when "_dired-make-symbolic-link"
     (add-hook 'dired-mode-hook
               (lambda ()
-                 (define-key dired-mode-map "S" (function dired-make-symbolic-link))
-                 )))
+                (define-key dired-mode-map "S" (function dired-make-symbolic-link))
+                )))
   (my-load-and-when "_dired-winstart"
     (add-hook 'dired-mode-hook
               (lambda ()
                 (define-key dired-mode-map "z" 'uenox-dired-winstart))))
   )
-
-
-;; -*-no-byte-compile: t; -*-
