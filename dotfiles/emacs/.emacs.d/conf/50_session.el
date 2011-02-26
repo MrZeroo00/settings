ffc63b544e3bdf753cc45f9f07113a1371ca5793
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-02-26 11:30:51 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1249 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/50_session.el
+++ dotfiles/emacs/.emacs.d/conf/50_session.el
@@ -7,4 +7,14 @@
                                   (file-name-history 10000)))
   (setq session-globals-max-string 100000000)
   (setq session-undo-check -1)
+
+  ;; exclude some files
+  ;; http://www.callcc.net/diary/20101106.html
+  (setq session-set-file-name-exclude-regexp "/\\.overview\\|.session\\|News/\\|COMMIT_EDITMSG")
+  (setq session-file-alist-exclude-regexp session-set-file-name-exclude-regexp)
+  (defadvice session-store-buffer-places (around apply-session-file-alist-exclude-regexp activate)
+    (let ((file-name (session-buffer-file-name)))
+      (when (and file-name
+                 (not (string-match session-file-alist-exclude-regexp file-name)))
+        ad-do-it)))
   (add-hook 'after-init-hook 'session-initialize))
