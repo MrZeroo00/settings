05be14a1ee311b963003cdb45007ff1af0b9b79f
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-02-27 13:30:04 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1363 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/50_window.el
+++ dotfiles/emacs/.emacs.d/conf/50_window.el
@@ -62,7 +62,9 @@
 (my-require-and-when 'popwin
   (setq display-buffer-function 'popwin:display-buffer)
   (setq anything-samewindow nil)
-  (add-to-list 'popwin:special-display-config '("*anything*" :position bottom :height 20)))
+  (add-to-list 'popwin:special-display-config '("*anything*" :position bottom :height 20))
+  (add-to-list 'popwin:special-display-config '("\\*grep\\*.*" :regexp t :position bottom :height 20))
+  )
 
 
 ;;;; winhist
