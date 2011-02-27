0811875309a13d22b2012d7a3a3354e1f97235c7
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-02-27 07:47:15 +0000

    cleanup
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1321 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/40_coding.el
+++ dotfiles/emacs/.emacs.d/conf/40_coding.el
@@ -245,29 +245,6 @@
   (setq jaspace-modes nil))
 
 
-;;;; emphasize Space/Tab/Newline
-;;;; http://www.bookshelf.jp/cgi-bin/goto.cgi?file=meadow&node=visualize%20tab
-;;;(defface my-face-r-1 '((t (:background "gray15"))) nil)
-;;;(defface my-face-b-1 '((t (:background "gray"))) nil)
-;;;(defface my-face-b-2 '((t (:background "gray26"))) nil)
-;;;(defface my-face-u-1 '((t (:foreground "SteelBlue" :underline t))) nil)
-;;;(defvar my-face-r-1 'my-face-r-1)
-;;;(defvar my-face-b-1 'my-face-b-1)
-;;;(defvar my-face-b-2 'my-face-b-2)
-;;;(defvar my-face-u-1 'my-face-u-1)
-;;;
-;;;(defadvice font-lock-mode (before my-font-lock-mode ())
-;;;  (font-lock-add-keywords
-;;;   major-mode
-;;;   '(("\t" 0 my-face-b-2 append)
-;;;     ("　" 0 my-face-b-1 append)
-;;;     ("[ \t]+$" 0 my-face-u-1 append)
-;;;     ;;("[\r]*\n" 0 my-face-r-1 append)
-;;;     )))
-;;;(ad-enable-advice 'font-lock-mode 'before 'my-font-lock-mode)
-;;;(ad-activate 'font-lock-mode)
-
-
 ;;;; face-list
 ;;;; http://groups.google.com/group/gnu.emacs.sources/msg/06afad63bfa99322
 ;;;(my-require-and-when 'face-list)
--- dotfiles/emacs/.emacs.d/conf/_obsolete/40_coding.el
+++ dotfiles/emacs/.emacs.d/conf/_obsolete/40_coding.el
@@ -2,6 +2,29 @@
 ;(my-require-and-when 'indent-tabs-maybe)
 
 
+;;;; emphasize Space/Tab/Newline
+;;;; http://www.bookshelf.jp/cgi-bin/goto.cgi?file=meadow&node=visualize%20tab
+;;;(defface my-face-r-1 '((t (:background "gray15"))) nil)
+;;;(defface my-face-b-1 '((t (:background "gray"))) nil)
+;;;(defface my-face-b-2 '((t (:background "gray26"))) nil)
+;;;(defface my-face-u-1 '((t (:foreground "SteelBlue" :underline t))) nil)
+;;;(defvar my-face-r-1 'my-face-r-1)
+;;;(defvar my-face-b-1 'my-face-b-1)
+;;;(defvar my-face-b-2 'my-face-b-2)
+;;;(defvar my-face-u-1 'my-face-u-1)
+;;;
+;;;(defadvice font-lock-mode (before my-font-lock-mode ())
+;;;  (font-lock-add-keywords
+;;;   major-mode
+;;;   '(("\t" 0 my-face-b-2 append)
+;;;     ("　" 0 my-face-b-1 append)
+;;;     ("[ \t]+$" 0 my-face-u-1 append)
+;;;     ;;("[\r]*\n" 0 my-face-r-1 append)
+;;;     )))
+;;;(ad-enable-advice 'font-lock-mode 'before 'my-font-lock-mode)
+;;;(ad-activate 'font-lock-mode)
+
+
 ;;;; xcscope
 ;;;; http://cscope.sourceforge.net/
 ;;;(my-require-and-when 'xcscope)
