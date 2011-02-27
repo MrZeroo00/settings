8bfdce2ff9ea34f37c8ad7ea23a87bfdc7d896fa
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-02-27 06:16:21 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1300 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/50_modeline.el
+++ dotfiles/emacs/.emacs.d/conf/50_modeline.el
@@ -1,15 +1,15 @@
 (line-number-mode t)
 (column-number-mode t)
+(setq eol-mnemonic-dos "(CRLF)")
+(setq eol-mnemonic-mac "(CR)")
+(setq eol-mnemonic-unix "(LF)")
 
 ;;;; display time
 (setq display-time-string-forms
-      '(month "/" day " " dayname " " 
+      '(month "/" day " " dayname " "
               24-hours ":" minutes " "
               (if mail " Mail" "") ))
 (display-time-mode t)
 
 ;;;; show current directory
 (add-to-list 'global-mode-string '("" default-directory "-"))
-
-
-;; -*-no-byte-compile: t; -*-
