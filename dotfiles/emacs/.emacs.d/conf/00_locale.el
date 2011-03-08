385a0afb5bd393890dd5f8905599735429e7980b
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-03-08 00:51:10 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1452 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/00_locale.el
+++ dotfiles/emacs/.emacs.d/conf/00_locale.el
@@ -4,22 +4,6 @@
 '(set-language-environment 'utf-8)
 '(prefer-coding-system 'utf-8)
 
-;;; same as prefer-coding-system
-'(set-default-coding-systems 'utf-8)
-'(set-terminal-coding-system 'utf-8)
-'(set-keyboard-coding-system 'utf-8)
-'(set-buffer-file-coding-system 'utf-8)
-'(setq default-buffer-file-coding-system 'utf-8)
-'(set-file-name-coding-system 'utf-8)
-'(setq default-process-coding-system
-       '(utf-8 . utf-8))
-
-'(set-buffer-process-coding-system 'utf-8 'utf-8)
-'(set-clipboard-coding-system 'utf-8)
-'(add-hook 'find-file-hooks
-           (lambda ()
-             (setq coding-system-for-read 'utf-8)
-             (setq coding-system-for-write 'utf-8)))
 '(cond
   (run-darwin
    (my-require-and-when 'ucs-normalize
@@ -40,6 +24,23 @@
    (setq file-name-coding-system 'utf-8)
    (setq locale-coding-system 'utf-8)
    ))
+
+;;; same as prefer-coding-system
+'(set-default-coding-systems 'utf-8)
+'(set-terminal-coding-system 'utf-8)
+'(set-keyboard-coding-system 'utf-8)
+'(set-buffer-file-coding-system 'utf-8)
+'(setq default-buffer-file-coding-system 'utf-8)
+'(set-file-name-coding-system 'utf-8)
+'(setq default-process-coding-system
+       '(utf-8 . utf-8))
+
+'(set-buffer-process-coding-system 'utf-8 'utf-8)
+'(set-clipboard-coding-system 'utf-8)
+'(add-hook 'find-file-hooks
+           (lambda ()
+             (setq coding-system-for-read 'utf-8)
+             (setq coding-system-for-write 'utf-8)))
 
 
 ;;;; locale setting
