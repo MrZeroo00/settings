15be9b8710985be90429b0a9b6880a30ec8506c0
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-03-08 00:50:18 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1451 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/00_locale.el
+++ dotfiles/emacs/.emacs.d/conf/00_locale.el
@@ -1,18 +1,21 @@
 ;;;; language setting
 '(my-require-and-when 'un-define)
 '(set-language-environment "Japanese")
+'(set-language-environment 'utf-8)
+'(prefer-coding-system 'utf-8)
+
+;;; same as prefer-coding-system
 '(set-default-coding-systems 'utf-8)
 '(set-terminal-coding-system 'utf-8)
 '(set-keyboard-coding-system 'utf-8)
 '(set-buffer-file-coding-system 'utf-8)
 '(setq default-buffer-file-coding-system 'utf-8)
 '(set-file-name-coding-system 'utf-8)
-'(set-buffer-process-coding-system 'utf-8 'utf-8)
 '(setq default-process-coding-system
        '(utf-8 . utf-8))
+
+'(set-buffer-process-coding-system 'utf-8 'utf-8)
 '(set-clipboard-coding-system 'utf-8)
-'(set-language-environment 'utf-8)
-'(prefer-coding-system 'utf-8)
 '(add-hook 'find-file-hooks
            (lambda ()
              (setq coding-system-for-read 'utf-8)
