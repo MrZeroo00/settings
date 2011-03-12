7786e93486eeff0b2160eac6bfe979aec459ec7d
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-03-12 17:49:17 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1486 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/vimperator/.vimperator/conf/default.d/00_plugin.js
+++ dotfiles/vimperator/.vimperator/conf/default.d/00_plugin.js
@@ -2,67 +2,168 @@ liberator.globalVariables.plugin_loader_roots = '~/.vimperator/_plugin/coderepos
 liberator.globalVariables.plugin_loader_plugins = [
     '_libly',
     '_smooziee',
-    //'appendAnchor',
-    //'applauncher',
-    'copy',
-    //'lo',
-    //'loginManager',
-    'multi_requester',
-    'nextlink',
-    //'toggler',
-    //'xpathBlink',
 
     // Bookmark
     'commandBookmarklet',
-    'direct_bookmark',
+    //'literal-bmark',
 
     // Cookie
     //'cookie',
     //'cookieManager',
 
+    // CSS
+    //'stylish',
+
+    // Development
+    //'auto_reload',
+    //'auto_source',
+    //'inspector',
+    //'jquery-loader',
+
+    // Download
+    //'simg',
+
     // Key
     'feedSomeKeys_3',
     'ime_controller',
     //'localkeymode',
+    //'yetmappings',
 
     // Hint
+    //'asdfghjkl',
     //'caret-hint',
     //'char-hints-mod2',
+    //'hints-for-embedded',
+    //'hints-yank-paste',
+    //'x-hint',
+    //'xpath_hint',
+
+    // Insert Mode
+    //'option-selector'
+    //'resizable_textarea'
+    //'walk-input',
+
+    // Javascript
+    //'garbage_finder',
+
+    // Login
+    //'account_switcher',
+    //'loginManager',
 
     // Menu
     'command_menu',
     //'tinymenu',
 
+    // Proxy
+    //'autoproxychanger',
+
+    // Setting
+    //'adddialog',
+    //'appinfo',
+    //'clear_privacy_data',
+    //'edit-vimperator-files',
+    //'extension-manager',
+    //'mkcolor',
+    //'options-migrate-user-pref',
+    //'pluginManager',
+    //'reporter',
+
+    // Status Bar
+    //'statstat',
+    'statusbar_panel',
+
+    // Tab
+    //'open-frame',
+    'panorama',
+    //'removetabs',
+    //'tabsort',
+
+    // URL
+    //'auto_detect_link',
+    //'appendAnchor',
+    //'exopen',
+    //'lo',
+    //'link-targets',
+    'opener',
+
     // Utility
     //'alert',
+    //'alias',
+    //'applauncher',
     'auto-focus-frame',
     'auto_word_select_mode',
+    //'browser_object',
+    'copy',
     'encodingSwitcherCommand',
+    //'every',
+    'history-search-backward',
+    //'imageextender',
     //'maine_coon',
-    //'link-targets',
+    //'marker_reader',
+    //'memo',
+    //'multi-exec',
+    'multi_requester',
+    'nextlink',
+    //'no-reading',
+    //'onclick',
+    //'refcontrol',
+    //'scenario-actor',
+    //'spatial-navigation',
+    //'toggler',
+    //'uaSwitchLite',
+    //'xpathBlink',
+    //'zoom-em-all',
 
     // Video
     //'stella',
-    //'video-controller'
+    //'video-controller',
+
+    // AutoPagerize
+    //'autopagerize_controll',
 
     // Fox Splitter
     //'splitBrowser',
+
+    // FoxyTunes
+    //'foxytunes',
+
+    // Greasemonkey
+    //'gmperator',
 
     // LDRize
     //'ldrize_cooperation',
     //'ldrize_cooperation_fetch_flv',
 
+    // Migemo
+    //'migemized_find',
+    'migemo-find',
+
+    // Session Manager
+    //'session-manager',
+
     // Tombloo
     'tombloo',
+    //'hint-tombloo',
 
     // Tree Style Tab
     //'treeStyleTab',
 
+    // Amazon
+    //'amazon_simple_uri',
+
     // Android
     //'vimp_to_android_phone',
 
+    // Gmail
+    //'gmail-commando',
+
+    // SBM
+    'direct_bookmark',
+    //'sbmcommentsviewer',
+
     // Twitter
-    'twittperator',
+    //'retweet',
+    //'twittperator',
 
     // Tumblr
     //'logintumblr',
