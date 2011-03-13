404499e93f55ce070ab7d89a02ffbcb054702cb3
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-03-13 20:51:18 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1508 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/70_css.el
+++ dotfiles/emacs/.emacs.d/conf/70_css.el
@@ -6,4 +6,16 @@
 (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
 
 
-;; -*-no-byte-compile: t; -*-
+;;;; mode hook
+(defun my-css-mode-hook ()
+  ;; common setting
+  (setq comment-style 'multi-line)
+
+  ;; imenu
+  (setq imenu-generic-expression '(("Selectors" "^[[:blank:]]*\\(.*[^ ]\\) *{" 1)))
+  (setq imenu-case-fold-search nil)
+  (setq imenu-auto-rescan t)
+  (setq imenu-space-replacement " ")
+  (imenu-add-menubar-index)
+  )
+(add-hook 'css-mode-hook 'my-css-mode-hook)
