918b95aa9229a0382338c95d53054f8ee95cc540
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-04-03 16:59:23 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1622 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/00_color.el
+++ dotfiles/emacs/.emacs.d/conf/00_color.el
@@ -10,6 +10,17 @@
   (set-face-background 'highlight "blue"))
 
 
+;;;; ediff
+(ediff-odd-diff-face-A ((t (:background "black" :foreground "white"))))
+(ediff-odd-diff-face-Ancestor ((t (:background "black" :foreground "white"))))
+(ediff-odd-diff-face-B ((t (:background "black" :foreground "white"))))
+(ediff-odd-diff-face-C ((t (:background "black" :foreground "white"))))
+(ediff-even-diff-face-A ((t (:background "black" :foreground "white"))))
+(ediff-even-diff-face-Ancestor ((t (:background "black" :foreground "white"))))
+(ediff-even-diff-face-B ((t (:background "black" :foreground "white"))))
+(ediff-even-diff-face-C ((t (:background "black" :foreground "white"))))
+
+
 ;;;; term
 (setq term-default-fg-color "#FFFFFF")
 (setq term-default-bg-color "#000000")
