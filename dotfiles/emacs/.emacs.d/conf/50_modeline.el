d6be432de817a3d454d42b3c3f6e8c29b34ee50e
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-04-03 15:47:25 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1613 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/50_modeline.el
+++ dotfiles/emacs/.emacs.d/conf/50_modeline.el
@@ -13,3 +13,28 @@
 
 ;;;; show current directory
 (add-to-list 'global-mode-string '("" default-directory "-"))
+
+;;;; format
+(setq mode-line-format '("%e"
+                         #("-" 0 1
+                           (help-echo "mouse-1: Select (drag to resize)\nmouse-2: Make current window occupy the whole frame\nmouse-3: Remove current window from display"))
+                         mode-line-mule-info mode-line-client mode-line-modified mode-line-remote mode-line-frame-identification mode-line-buffer-identification
+                         #("   " 0 3
+                           (help-echo "mouse-1: Select (drag to resize)\nmouse-2: Make current window occupy the whole frame\nmouse-3: Remove current window from display"))
+                         mode-line-position
+                         (vc-mode vc-mode)
+                         #("  " 0 2
+                           (help-echo "mouse-1: Select (drag to resize)\nmouse-2: Make current window occupy the whole frame\nmouse-3: Remove current window from display"))
+                         mode-line-modes
+                         (which-func-mode
+                          ("" which-func-format
+                           #("--" 0 2
+                             (help-echo "mouse-1: Select (drag to resize)\nmouse-2: Make current window occupy the whole frame\nmouse-3: Remove current window from display"))))
+                         (global-mode-string
+                          (#("--" 0 2
+                             (help-echo "mouse-1: Select (drag to resize)\nmouse-2: Make current window occupy the whole frame\nmouse-3: Remove current window from display"))
+                           global-mode-string))
+                         #("-%-" 0 3
+                           (help-echo "mouse-1: Select (drag to resize)\nmouse-2: Make current window occupy the whole frame\nmouse-3: Remove current window from display")))
+      )
+(setq mode-line-frame-identification " ")
