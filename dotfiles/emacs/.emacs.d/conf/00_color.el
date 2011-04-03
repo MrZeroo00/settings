af5cd8a670b38ecc872c96c8e345c4a2a83fe7e7
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-04-03 17:06:05 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1625 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/00_color.el
+++ dotfiles/emacs/.emacs.d/conf/00_color.el
@@ -11,10 +11,10 @@
 
 
 ;;;; ediff
-(setq ediff-odd-diff-face-A ((t (:background "black" :foreground "white"))))
-(setq ediff-even-diff-face-A ((t (:background "black" :foreground "white"))))
-(setq ediff-odd-diff-face-B ((t (:background "black" :foreground "white"))))
-(setq ediff-even-diff-face-B ((t (:background "black" :foreground "white"))))
+(setq ediff-odd-diff-face-A ((t (:background "magenta" :foreground "white"))))
+(setq ediff-even-diff-face-A ((t (:background "magenta" :foreground "white"))))
+(setq ediff-odd-diff-face-B ((t (:background "blue" :foreground "white"))))
+(setq ediff-even-diff-face-B ((t (:background "blue" :foreground "white"))))
 (setq ediff-odd-diff-face-C ((t (:background "black" :foreground "white"))))
 (setq ediff-even-diff-face-C ((t (:background "black" :foreground "white"))))
 (setq ediff-odd-diff-face-Ancestor ((t (:background "black" :foreground "white"))))
