5566a0021edccbb1aadf4611bdd1675957846389
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-05-10 18:26:55 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1649 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/70_php.el
+++ dotfiles/emacs/.emacs.d/conf/70_php.el
@@ -6,7 +6,8 @@
       "Use php to check the syntax of the current file."
       (let* ((temp (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))
              (local (file-relative-name temp (file-name-directory buffer-file-name))))
-        (list "php" (list "-f" local "-l"))))
+        ;;(list "php" (list "-f" local "-l"))))
+        (list "php" (list "-n -f" local "-l"))))
 
     (add-to-list 'flymake-err-line-patterns
                  '("\\(Parse\\|Fatal\\) error: +\\(.*?\\) in \\(.*?\\) on line \\([0-9]+\\)$" 3 4 nil 2))
