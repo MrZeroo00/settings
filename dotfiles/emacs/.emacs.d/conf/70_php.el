e69746b6781afd37db2bd9c9246228ef4f2505d8
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-07-28 17:43:04 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1681 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/70_php.el
+++ dotfiles/emacs/.emacs.d/conf/70_php.el
@@ -98,6 +98,8 @@
 
   ;; php.log
   (setq compile-command "tac /var/log/php.log | tail -n 100")
+  (add-to-list 'compilation-error-regexp-alist '("in \\(.*?\\) on line \\([0-9]+\\)$" 1 2))
+  (add-to-list 'compilation-error-regexp-alist '(" \\([^ ]*?\\):\\([0-9]+\\)$" 1 2))
   )
 
 
