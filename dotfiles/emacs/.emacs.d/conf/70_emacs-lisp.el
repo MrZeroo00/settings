27f3bf2f28bdcff37f2315376117e3d560e0b2af
tanda <tanda@56156b2c-8c97-4338-b0ae-4de5fc0ae9d3>
2011-12-29 04:18:22 +0000

    update.
    git-svn-id: http://tanda.unfuddle.com/svn/tanda_settings@1719 56156b2c-8c97-4338-b0ae-4de5fc0ae9d3

--- dotfiles/emacs/.emacs.d/conf/70_emacs-lisp.el
+++ dotfiles/emacs/.emacs.d/conf/70_emacs-lisp.el
@@ -6,7 +6,7 @@
                                       anything-c-source-lisp-complete-symbol
                                       anything-c-source-linkd-tag
                                       )))
-	(add-to-list anything-kyr-commands-by-major-mode
+	(add-to-list 'anything-kyr-commands-by-major-mode
 				 '(emacs-lisp-mode
 				   byte-compile-file
 				   ))
