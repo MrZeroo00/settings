268254e5dc57b53d0159a88ce3aebde29b1787a7
Mitsuhiro Tanda <mitsuhiro.tanda@gree.co.jp>
2012-07-28 15:11:58 +0900

    update.

--- shellscripts/emacs/profile.sh
+++ shellscripts/emacs/profile.sh
@@ -4,5 +4,7 @@ for i in `ls ${HOME}/.emacs.d/conf/*.el`; do
   elisp=`echo $i | perl -pe "s/.*\/(.*)\.el/\1/"`
   echo $elisp
   emacsclient -e "(my-load-and-when \"${elisp}\")"
-  time emacsclient -e '(load-test "foo\nbar\nbaz" 10000)'
+  sleep 1
+  time emacsclient -e '(load-test "if\nfor\nwhile" 100000)'
+  sleep 1
 done
