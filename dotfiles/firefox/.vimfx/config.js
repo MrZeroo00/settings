3563f8283380d0b9f1f53b6b0e7b647d80413e24
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2016-12-14 01:04:40 +0900

    fix github keymap

--- dotfiles/firefox/.vimfx/config.js
+++ dotfiles/firefox/.vimfx/config.js
@@ -3,7 +3,7 @@ vimfx.set('hint_chars', 'asdfgwertxcvb')
 vimfx.set('prevent_autofocus', true)
 
 vimfx.addKeyOverrides(
-  [ location => location.hostname === 'github.com',
-    ['j', 'k', 't']
+  [ location => (location.hostname === "github.com" || location.hostname.match(/^git\..*\.net$/)),
+    ['j', 'k', 'l', 's', 't', 'w', 'y', '?']
   ]
 )
